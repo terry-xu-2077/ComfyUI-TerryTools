bac012f1a96b12a031fcbbdf22e72fd9d68e7457
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-08-30 10:28:39 +0800

    feat: export line switch node

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -2,6 +2,7 @@ from .date_formatter import DateFormatter
 from .file_save import FileSave
 from .h3_prompt_editor import H3PromptEditor
 from .h3_shot_timeline import H3ShotTimeline
+from .line_switch import LineSwitch
 from .video_compare import VideoCompare
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "VideoCompare",
     "H3PromptEditor",
     "H3ShotTimeline",
+    "LineSwitch",
 ]
